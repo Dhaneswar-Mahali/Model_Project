981fb81dea3f3d7e0c80ea470ac16191d1941aeb
Dhaneswar-Mahali <mahalidhaneswar2@gmail.com>
2022-07-11 06:32:00 +0530

    Initial commit

---
+++ Model_App/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ModelAppConfig(AppConfig):
+    name = 'Model_App'
---
+++ Model_App/models.py
@@ -0,0 +1,20 @@
+'''
+1. import the  models module
+
+2. create  userdefined model class   by using predefind  model class
+
+3. create required userdefined model fields with required field datatypes.
+
+'''
+
+from django.db import models
+class Employee(models.Model):
+    eno = models.IntegerField(primary_key=True)
+    ename = models.CharField(max_length=30)
+    salary = models.FloatField()
+
+
+
+
+
+
